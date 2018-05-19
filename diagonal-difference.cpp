61a5cda80af1d7556774f08fccb50569ea1cf676
salmandotexe <salman.exe@gmail.com>
2018-05-19 20:14:42 +0600

    Create diagonal-difference.cpp

---
+++ diagonal-difference.cpp
@@ -0,0 +1,12 @@
+int diagonalDifference(vector<vector<int>> a) {
+    int rem=0;
+    int s=a.size();
+    for(int i=0;i<s;i++)
+    {
+        rem+=a[i][i];
+        rem-=a[s-i-1][i];
+    }
+    
+    return (rem<0?-rem:rem);
+
+}
