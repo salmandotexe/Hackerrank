1297de22fd58b82526c06e86067cf8c6fbb46f9a
salmandotexe <salman.exe@gmail.com>
2018-05-24 18:27:00 +0600

    Insert a Node at the Tail of a Linked List

---
+++ InsertNodeatTail.cpp
@@ -0,0 +1,27 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Insert(Node *head,int data)
+{
+    Node *n=new Node();
+    n->data=data;
+    n->next=NULL;
+    if(head==NULL)
+    {
+        return n;
+    }
+    Node *ptr=head;
+    while(ptr->next!=NULL)
+        ptr=ptr->next;
+    ptr->next=n;
+    return head;
+    
+    
+}
