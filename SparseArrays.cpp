a2cfbe5661a037ff329ac5dbad1f2c362a189793
salmandotexe <salman.exe@gmail.com>
2018-05-26 19:11:19 +0600

    Create SparseArrays.cpp

---
+++ SparseArrays.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+// Complete the matchingStrings function below.
+
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    vector<int> res;
+    for(auto a:queries)
+    {
+        int count=0;
+        for(auto b:strings)
+        {
+            if(a==b)
+                count++;
+        }
+        res.push_back(count);
+    }
+    return res;
+}
